9b820f905a5dfeeb8c69582ab3b1e9cd63eb2cde
abarnakrishnamoorthi <124656007+abarnakrishnamoorthi@users.noreply.github.com>
2025-12-05 21:43:21 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Hero.css';
 import pic from '../../assets/22ITR001-new.jpg';
+
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Hero = () => {
